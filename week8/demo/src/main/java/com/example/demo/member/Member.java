7bb2d5e57c939762a717bddb078dae389f86bdcc
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-18 15:24:06 +0900

    실습 2-4: @Getter - 클래스 내 필드 단위 적용

--- week8/demo/src/main/java/com/example/demo/member/Member.java
+++ week8/demo/src/main/java/com/example/demo/member/Member.java
@@ -3,6 +3,7 @@ package com.example.demo.member;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
@@ -10,7 +11,9 @@ public class Member {
 
   private Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL 이 들어갈 수 있다.
   private String name;
+  @Setter
   private int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
+  @Setter
   private String email;
 
   public String toString() {
