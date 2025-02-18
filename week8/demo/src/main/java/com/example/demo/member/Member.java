bd179578dbd3d681529a1ecdc6c60a13a45942fe
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-18 15:42:18 +0900

    실습 2-12: (심화, 잘안씀) @ToString 표기 순서 및 이름 지정

--- week8/demo/src/main/java/com/example/demo/member/Member.java
+++ week8/demo/src/main/java/com/example/demo/member/Member.java
@@ -11,9 +11,9 @@ import lombok.ToString;
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class Member {
 
-  @ToString.Include
+  @ToString.Include(name = "memberId")
   private Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL 이 들어갈 수 있다.
-  @ToString.Include
+  @ToString.Include(rank = -1) // 출력순위로 0보다 낮을수록 더 후순위
   private String name;
   @Setter(AccessLevel.PRIVATE)
   private int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
