af901c13ad26f71564fad8ca3f65431eda31af1f
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-18 15:19:35 +0900

    실습 2-2: @AllArgsConstructor - 생성자 접근제어자 옵션 설정 가능

--- week8/demo/src/main/java/com/example/demo/member/Member.java
+++ week8/demo/src/main/java/com/example/demo/member/Member.java
@@ -1,15 +1,16 @@
 package com.example.demo.member;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class Member {
 
   protected Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL 이 들어갈 수 있다.
   protected String name;
   protected int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
   protected String email;
-  
+
   public String toString() {
     return String.format("Member[id=%d, name=%s, email=%s]", id, name, email);
   }
