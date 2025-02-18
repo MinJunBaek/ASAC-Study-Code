aba32c3cda5e2e6579f7c6b6af2170adee54c0c9
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-18 15:28:24 +0900

    실습 2-6: 객체의 toString은 재선언하지 않는 이상 주소 표시

--- week8/demo/src/main/java/com/example/demo/member/Member.java
+++ week8/demo/src/main/java/com/example/demo/member/Member.java
@@ -15,8 +15,5 @@ public class Member {
   private int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
   @Setter
   private String email;
-
-  public String toString() {
-    return String.format("Member[id=%d, name=%s, email=%s]", id, name, email);
-  }
+  
 }
