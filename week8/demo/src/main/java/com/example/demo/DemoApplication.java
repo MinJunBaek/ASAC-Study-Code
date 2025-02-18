1041460a9a4d7996fad7bb965f019cec42b2ab5e
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-18 10:16:24 +0900

    실습 1-9: 빈 생성자(@NoArgsConstructor) + 수정자(@Setter) 통한 필드 주입

--- week8/demo/src/main/java/com/example/demo/DemoApplication.java
+++ week8/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,20 +8,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
   public static void main(String[] args) {
-    Administrator aaron = new Administrator("Aaron", "aaron@example.com", "DEVELOPER");
-    Administrator baron = new Administrator("Baron", "baron@example.com", "DESIGNER");
+    Member aaron = new Member();
+    aaron.setName("Aaron");
+    aaron.setEmail("aaron@example.com");
+    Member baron = new Member();
+    aaron.setName("Baron");
+    aaron.setEmail("baron@example.com");
 
     System.out.println("---");
     System.out.println(aaron);              // 객체
     System.out.println(aaron.toString());   // 객체
     System.out.println(aaron.getName());    // 필드 중 name
-//      System.out.println(aaron.name);         // 필드 중 name (접근제어자 private)
+//      System.out.println(aaron.name);         // 필드 중 name (접근제어자 protected)
 
     System.out.println("---");
     System.out.println(baron);              // 객체
     System.out.println(baron.toString());   // 객체
     System.out.println(baron.getName());    // 필드 중 name
-//      System.out.println(baron.name);         // 필드 중 name (접근제어자 private)
+//      System.out.println(baron.name);         // 필드 중 name (접근제어자 protected)
   }
 
 }
