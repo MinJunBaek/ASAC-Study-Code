806e62ce86789eb665435e3bc03231858409ecff
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-19 22:22:09 +0900

    실습 1-17: (심화) 빌더(@Builder) 사용 시 - (1) 클래스 단위가 아닌 생성자 단위 사용

--- week8/demo/src/main/java/com/example/demo/member/Administrator.java
+++ week8/demo/src/main/java/com/example/demo/member/Administrator.java
@@ -18,7 +18,7 @@ public class Administrator extends Member {
   LocalDate allocatedAt;
 
   public Administrator(Integer id, String name, int age, String email, String role) {
-    super(id, name, age, email);
+    super(name, email);
     this.role = role;
     this.allocatedAt = LocalDate.now();
   }
