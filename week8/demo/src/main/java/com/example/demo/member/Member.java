63b498beed9ae7898852478aa45b8c45a4672470
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-19 10:09:12 +0900

    실습 2-24: @EqualsAndHashCode.Exclude - 비교안할 필드 제외

--- week8/demo/src/main/java/com/example/demo/member/Member.java
+++ week8/demo/src/main/java/com/example/demo/member/Member.java
@@ -13,14 +13,16 @@ import lombok.ToString;
 @Getter
 @ToString(onlyExplicitlyIncluded = true, includeFieldNames = false)
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
-@EqualsAndHashCode(exclude = {"age", "email"})
+@EqualsAndHashCode
 public class Member {
 
   @ToString.Include(name = "memberId")
   private Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL 이 들어갈 수 있다.
   @ToString.Include(rank = -1) // 출력순위로 0보다 낮을수록 더 후순위
   private String name;
+  @EqualsAndHashCode.Exclude
   private int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
+  @EqualsAndHashCode.Exclude
   private String email;
 
 //  public boolean equals(Member member) {
