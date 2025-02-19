b06d6477b25907a626ffbd44a4a98bbedea5e1ff
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-19 10:40:04 +0900

    실습 2-30: @FieldDefaults - 클래스 내 필드 일일히 접근제어자 설정이 귀찮을때

--- week8/demo/src/main/java/com/example/demo/member/Administrator.java
+++ week8/demo/src/main/java/com/example/demo/member/Administrator.java
@@ -1,17 +1,21 @@
 package com.example.demo.member;
 
 import java.time.LocalDate;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 
 // @AllArgsConstructor
 @ToString(callSuper = true) // 상속받아도 자식 클래스의 필드만 노출 callSuper를 통해 부모 클래스의 필드 노출
 @EqualsAndHashCode(callSuper = true) // Equals 상속
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class Administrator extends Member {
 
-  private String role;
-  private LocalDate allocatedAt;
+  String role;
+  LocalDate allocatedAt;
 
   public Administrator(Integer id, String name, int age, String email, String role) {
     super(id, name, age, email);
--- week8/demo/src/main/java/com/example/demo/member/Member.java
+++ week8/demo/src/main/java/com/example/demo/member/Member.java
@@ -8,22 +8,24 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.experimental.FieldDefaults;
 
 @Setter
 @Getter
 @ToString(onlyExplicitlyIncluded = true, includeFieldNames = false)
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Member {
 
   @ToString.Include(name = "memberId")
   @EqualsAndHashCode.Include
-  private Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL 이 들어갈 수 있다.
+  Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL 이 들어갈 수 있다.
   @ToString.Include(rank = -1) // 출력순위로 0보다 낮을수록 더 후순위
   @EqualsAndHashCode.Include
-  private String name;
-  private int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
-  private String email;
+  String name;
+  int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
+  String email;
 
 //  public boolean equals(Member member) {
 //    return this.hashCode() == member.hashCode();
