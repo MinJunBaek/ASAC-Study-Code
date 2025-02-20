1909f6c7d7f692971a2a2c3f9332d6edf6d568c1
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-20 22:15:05 +0900

    실습 1-23: 정적 팩토리 메서드 - Private 생성자 위해 @AllArgsConstructor 옵션

--- week8/demo/src/main/java/com/example/demo/member/Member.java
+++ week8/demo/src/main/java/com/example/demo/member/Member.java
@@ -2,26 +2,21 @@ package com.example.demo.member;
 
 import com.example.demo.dto.MemberCreateRequestDto;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 import lombok.experimental.FieldDefaults;
 
 @ToString
-@FieldDefaults(level = AccessLevel.PROTECTED)
 @Getter
+@FieldDefaults(level = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Member {
 
   Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL 이 들어갈 수 있다.
   String name = "Unnamed";
   int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
   String email = "Undefined";
-
-  private Member(Integer id, String name, int age, String eamil) {
-    this.id = id;
-    this.name = name;
-    this.age = age;
-    this.email = eamil;
-  }
 
   public static Member from(MemberCreateRequestDto requestDto) {
     return new Member(
