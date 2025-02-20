926862ae81ca49d6007056f63f4f4ef6cb585bf1
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-20 23:33:01 +0900

    실습 1-26: Race Condition 해결 - 2) 변수 생성 단위 Blocking : vloatile + synchronized

--- week8/demo/src/main/java/com/example/demo/member/Member.java
+++ week8/demo/src/main/java/com/example/demo/member/Member.java
@@ -9,17 +9,20 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Member {
 
-  private static Member instance;
+  // volatile 키워드를 사용하여 메모리 가시성 문제 방지 (CPU 캐시에서 다른 스레드가 오래된 데이터를 참조하는 문제 해결)
+  private volatile static Member instance;
 
   Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL 이 들어갈 수 있다.
   String name = "Unnamed";
   int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
   String email = "Undefined";
 
-  public synchronized static Member getInstance() {
+  public static Member getInstance() {
     // 체킹을 하게될시 블락을 해서 중복체킹을 방지
-    if (Objects.isNull(instance)) {
-      instance = new Member();
+    synchronized (Member.class) {
+      if (Objects.isNull(instance)) {
+        instance = new Member();
+      }
     }
     return instance;
   }
