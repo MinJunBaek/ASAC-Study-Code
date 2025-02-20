b373cfea89103ee8987e0d052793fe40989625eb
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-20 22:50:20 +0900

    실습 1-24: Singleton 싱글턴 객체를 위한 정적 팩토리 메서드

--- week8/demo/src/main/java/com/example/demo/member/Member.java
+++ week8/demo/src/main/java/com/example/demo/member/Member.java
@@ -1,29 +1,26 @@
 package com.example.demo.member;
 
-import com.example.demo.dto.MemberCreateRequestDto;
+import java.util.Objects;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
-@ToString
-@Getter
-@FieldDefaults(level = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Member {
+
+  private static Member instance;
 
   Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL 이 들어갈 수 있다.
   String name = "Unnamed";
   int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
   String email = "Undefined";
 
-  public static Member from(MemberCreateRequestDto requestDto) {
-    return new Member(
-        null,
-        requestDto.getName(),
-        0,
-        requestDto.getEmail()
-    );
+  public synchronized static Member getInstance() {
+    // 체킹을 하게될시 블락을 해서 중복체킹을 방지
+    if (Objects.isNull(instance)) {
+      instance = new Member();
+    }
+    return instance;
   }
 }
