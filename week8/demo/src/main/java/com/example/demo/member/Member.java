e9a4fb4c3e558c001429bcd95a7746b7020a083f
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-20 23:40:33 +0900

    실습 1-27: Race Condition 해결 - 3) 변수 사전 생성 : Lazy Holder

--- week8/demo/src/main/java/com/example/demo/member/Member.java
+++ week8/demo/src/main/java/com/example/demo/member/Member.java
@@ -1,29 +1,26 @@
 package com.example.demo.member;
 
-import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.experimental.FieldDefaults;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
 public class Member {
 
-  // volatile 키워드를 사용하여 메모리 가시성 문제 방지 (CPU 캐시에서 다른 스레드가 오래된 데이터를 참조하는 문제 해결)
-  private volatile static Member instance;
+  private static class LazyHolder {
+
+    private static final Member UNIQUE_INSTANCE = new Member();
+  }
 
   Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL 이 들어갈 수 있다.
-  String name = "Unnamed";
+  String name;
   int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
-  String email = "Undefined";
+  String email;
 
   public static Member getInstance() {
-    // 체킹을 하게될시 블락을 해서 중복체킹을 방지
-    synchronized (Member.class) {
-      if (Objects.isNull(instance)) {
-        instance = new Member();
-      }
-    }
-    return instance;
+    return LazyHolder.UNIQUE_INSTANCE;
   }
 }
