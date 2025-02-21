0e2885f396dcc2c823dc4675a066a786f298b0ce
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-21 12:20:33 +0900

    실습 1-31: Static 필드 - 객체에 귀속되지 않으니 내부 사용시 this 미사용

--- week8/demo/src/main/java/com/example/demo/DemoApplication.java
+++ week8/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,10 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
   public static void main(String[] args) {
-    Member aaron = new Member(1, "Aaron", 10, "aaron@example.com");
+    Member member = new Member(1, "Aaron", 10, "aaron@example.com");
 
     System.out.println("---");
-    System.out.println(aaron);              // 객체 주소 (aaron 주소값 == baron 주소값)
-    System.out.println(aaron.toString());   // 객체
+    System.out.println(member);              // 객체 주소 (aaron 주소값 == baron 주소값)
+    System.out.println(member.toString());   // 객체
+    System.out.println(member.MEMBER_TO_STRING_FORMAT);
+    System.out.println(Member.MEMBER_TO_STRING_FORMAT);
   }
 }
--- week8/demo/src/main/java/com/example/demo/member/Member.java
+++ week8/demo/src/main/java/com/example/demo/member/Member.java
@@ -1,21 +1,20 @@
 package com.example.demo.member;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.experimental.FieldDefaults;
 
 @Getter
-@FieldDefaults(level = AccessLevel.PRIVATE)
 @AllArgsConstructor
-public final class Member {
+public class Member {
 
-  Integer id;
-  String name;
-  int age;
-  String email;
+  public static String MEMBER_TO_STRING_FORMAT = "Member(id=%s, name=%s, age=%s, email=%s)";
 
-  public final String toString() {
-    return String.format("Member(id=%s, name=%s, age=%s, email=%s)", this.id, this.name, this.age, this.email);
+  private Integer id;
+  private String name;
+  private int age;
+  private String email;
+
+  public String toString() {
+    return String.format(MEMBER_TO_STRING_FORMAT, this.id, this.name, this.age, this.email);
   }
 }
