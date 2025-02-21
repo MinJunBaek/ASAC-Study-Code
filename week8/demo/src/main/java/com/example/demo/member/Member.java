1d4ac6cc987d4a8619c64aa2261ef77f6e259fcb
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-21 11:52:03 +0900

    실습 1-28: Final 필드 - 한번 생성자로 설정된 필드값은 Setter 호출 불가

--- week8/demo/src/main/java/com/example/demo/member/Member.java
+++ week8/demo/src/main/java/com/example/demo/member/Member.java
@@ -1,26 +1,20 @@
 package com.example.demo.member;
 
 import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.FieldDefaults;
 
-@FieldDefaults(level = AccessLevel.PRIVATE)
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @ToString
+@Setter(AccessLevel.PUBLIC)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
 public class Member {
 
-  private static class LazyHolder {
-
-    private static final Member UNIQUE_INSTANCE = new Member();
-  }
-
-  Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL 이 들어갈 수 있다.
-  String name;
-  int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
+  final Integer id;
+  final String name;
+  int age;
   String email;
 
-  public static Member getInstance() {
-    return LazyHolder.UNIQUE_INSTANCE;
-  }
 }
