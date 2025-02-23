a4ef51b705a9f236238f579887ea8da0a9f24a86
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-23 19:07:11 +0900

    실습 3-3: 인터페이스 사용 이유 - 인터페이스 다형성(Polymorphism)

--- week8/demo/src/main/java/com/example/demo/DemoApplication.java
+++ week8/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,20 +3,21 @@ package com.example.demo;
 import com.example.demo.hero.AgilityHero;
 import com.example.demo.hero.IntelligenceHero;
 import com.example.demo.hero.StrengthHero;
+import com.example.demo.hero.common.Hero;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
   public static void main(String[] args) {
-    StrengthHero strength = new StrengthHero();
-    AgilityHero agility = new AgilityHero();
-    IntelligenceHero intelligence = new IntelligenceHero();
+    Hero strength = new StrengthHero();
+    Hero agility = new AgilityHero();
+    Hero intelligence = new IntelligenceHero();
 
     System.out.println("- 🟥 힘 속성 영웅");
     strength.attack();
     strength.ultimate();
-    strength.berserk();
+    ((StrengthHero) strength).berserk();
 
     System.out.println("- 🟩 민첩 속성 영웅");
     agility.attack();
