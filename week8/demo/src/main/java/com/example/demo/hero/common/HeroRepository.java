6416654a47312c0dc0e176f1079f2e9a9fa4f84e
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-24 19:50:14 +0900

    실습 3-15: Interface - 다중 상속(interface extends interface(s))

--- week8/demo/src/main/java/com/example/demo/hero/common/HeroRepository.java
+++ week8/demo/src/main/java/com/example/demo/hero/common/HeroRepository.java
@@ -1,16 +1,6 @@
 package com.example.demo.hero.common;
 
-public interface HeroRepository<T extends Hero, F> {
+public interface HeroRepository<T extends Hero, F> extends HeroReadRepository<T, F>, HeroCreateRepository<T, F>,
+    HeroDeleteRepository<T, F> {
 
-  default T findById(F id) {
-    throw new RuntimeException("have to be implemented : T findById(F id)");
-  }
-
-  default F save(T hero) {
-    throw new RuntimeException("have to be implemented : F save(T hero)");
-  }
-
-  default void delete(F id) {
-    throw new RuntimeException("have to be implemented : void delete(F id)");
-  }
 }
