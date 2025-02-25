931083ff3dabf3ac55903dd15a41434ea7b5c600
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-26 05:12:01 +0900

    실습 0-1: Unchecked Exception - 1) Runtime 시 발생하는 Exception

--- week8/demo/src/main/java/com/example/demo/DemoApplication.java
+++ week8/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(DemoApplication.class, args);
+  static void connect(String username, String password) {
+    if (username.equals("admin") && password.equals("1234")) {
+      System.out.println("- Database is connected successfully.");
+    } else {
+      throw new RuntimeException("데이터베이스 접속 실패");
+    }
   }
-  
+
+  public static void main(String[] args) {
+    connect("admin", "1234");
+    connect("user", "9876");
+  }
 }
