16707df4d67886037fc2b7a093b624f5ee9e4660
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-25 16:38:10 +0900

    실습 3-23: Enum 사용 시(@어노테이션 사용) - 한정된 파라미터만 주입 가능

--- week8/demo/src/main/java/com/example/demo/type/MessageType.java
+++ week8/demo/src/main/java/com/example/demo/type/MessageType.java
@@ -1,18 +1,16 @@
 package com.example.demo.type;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public enum MessageType {
-  //  public static final String LOVE = "사랑합니다";
-  //  public static final String THANKS = "감사합니다";
   LOVE("사랑합니다."),
   THANKS("감사합니다.");
 
-  private final String message;
-
-  private MessageType(String message) {
-    this.message = message;
-  }
-
-  public String getMessage() {
-    return this.message;
-  }
+  String message;
 }
