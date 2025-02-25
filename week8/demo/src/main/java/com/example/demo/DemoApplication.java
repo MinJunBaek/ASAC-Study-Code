c045fb30440770a9053bb4614291d0c2c049291e
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-25 16:13:22 +0900

    실습 3-22: Enum 미사용 시 - 수많은 파라미터 주입 가능

--- week8/demo/src/main/java/com/example/demo/DemoApplication.java
+++ week8/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -12,48 +12,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+  public static void createMessage(String message) {
+    System.out.println("당신에게 메세지를 보냅니다. : " + message);
+  }
+
   public static void main(String[] args) {
-    StrengthHeroRepository strengthHeroRepository = new StrengthHeroRepository();
-    strengthHeroRepository.save(new StrengthHero("Dragon Knight"));
-    strengthHeroRepository.save(new StrengthHero("Pudge"));
-    strengthHeroRepository.save(new StrengthHero("Slardar"));
-    strengthHeroRepository.delete("Pudge");
-
-    AgilityHeroRepository agilityRepository = new AgilityHeroRepository();
-    agilityRepository.save(new AgilityHero("Weaver"));
-    agilityRepository.save(new AgilityHero("Slark"));
-    agilityRepository.save(new AgilityHero("Juggernaut"));
-    agilityRepository.delete("Weaver");
-
-    IntelligenceHeroRepository intelligenceRepository = new IntelligenceHeroRepository();
-    intelligenceRepository.save(new IntelligenceHero("Puck"));
-    intelligenceRepository.save(new IntelligenceHero("Crystal Maiden"));
-    intelligenceRepository.save(new IntelligenceHero("Zeus"));
-    intelligenceRepository.delete("Puck");
-
-    // Hero strength = strengthHeroRepository.findById("Slardar");
-    Hero strength = strengthHeroRepository.findById("Slardar");
-    // Hero agility = agilityRepository.findById("Slark");
-    Hero agility = agilityRepository.findById("Slark");
-    // Hero intelligence = intelligenceRepository.findById("Zeus");
-    Hero intelligence = intelligenceRepository.findById("Zeus");
-
-    System.out.println("- 🟥 힘 속성 영웅");
-    System.out.println(strength.getName());     // 인터페이스 다형성(Polymorphism) 상관없이 구체(구현)클래스 내 메서드 호출 가능
-    strength.heroAttack();
-    strength.heroUltimate();
-    ((StrengthHero) strength).heroBerserk(); // 인터페이스 다형성(Polymorphism) 상관없이 구체(구현)클래스 내 메서드 호출 가능
-
-    System.out.println("- 🟩 민첩 속성 영웅");
-    System.out.println(agility.getName());      // 인터페이스 다형성(Polymorphism) 상관없이 구체(구현)클래스 내 메서드 호출 가능
-    agility.heroAttack();
-    agility.heroUltimate();
-
-    System.out.println("- 🟦 지능 속성 영웅");
-    System.out.println(intelligence.getName()); // 인터페이스 다형성(Polymorphism) 상관없이 구체(구현)클래스 내 메서드 호출 가능
-    intelligence.heroAttack();
-    intelligence.heroUltimate();
-
+    createMessage("안녕하세요.");
+    createMessage("사랑합니다.");
+    createMessage("감사합니다.");
+    createMessage("안녕히가세요.");
   }
 
 }
