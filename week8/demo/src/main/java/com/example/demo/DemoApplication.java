22ae7844e2ae2a37eeb1309cb688e7a7f02c0cef
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-26 07:11:52 +0900

    실습 0-8: 예외 상황별 Exception 필드값 활용 + 단일 Exception 클래스 Catch

--- week8/demo/src/main/java/com/example/demo/DemoApplication.java
+++ week8/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,6 @@
 package com.example.demo;
 
-import com.example.demo.exception.DatabaseException;
-import com.example.demo.exception.PasswordException;
-import com.example.demo.exception.UsernameException;
-import java.sql.SQLException;
+import com.example.demo.exception.CustomException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.util.StringUtils;
@@ -13,32 +10,31 @@ import org.springframework.util.StringUtils;
 public class DemoApplication {
 
   static void connect(String username, String password) {
+    // StringUtils.hasLength()는 Spring Framework에서 제공하는 문자열 유틸리티 메서드로,
+    // 문자열이 비어있는지 확인할 때 사용, 공백("")만 있는 경우는 "길이가 있다"고 판단하지 않음
     if (!StringUtils.hasLength(username)) {
-      throw new UsernameException();
+      throw new CustomException("EMPTY_USERNAME", 2, "데이터베이스 접속에 필요한 Username 입력은 필수입니다.");
     }
     if (!StringUtils.hasLength(password)) {
-      throw new PasswordException();
+      throw new CustomException("EMPTY_PASSWORD", 2, "데이터베이스 접속에 필요한 Password 입력은 필수입니다.");
     }
     if (!(username.equals("admin") && password.equals("1234"))) {
-      throw new DatabaseException();
+      throw new CustomException("LOGIN_FAILED", 1, "데이터베이스 접속 실패");
+    }
+    if (true /* 강제로 예외를 발생시키기 위해 if (true) 추가 - 그렇지않으면 '아래부터 실행되지 않는데 코드가 있다'는 빌드 오류 */) {
+      throw new CustomException("CONNECTION_ERROR", 0, "Cannot connect to the Database Server");
     }
     log.info("- Database is connected successfully.");
   }
 
   public static void main(String[] args) { // throws SQLException 발생 가능성 선언과 같다
     try {
-      // connect(null, "9876");
-      // connect("user", null);
-      // connect("user", "9876");
+//      connect(null, "9876");
+//      connect("user", null);
+//      connect("user", "9876");
       connect("admin", "1234");
-    } catch (UsernameException e) {
-      log.warn("유저가 존재하지 않는 아이디를 입력 중", e);
-    } catch (PasswordException e) {
-      log.warn("유저가 잘못된 비밀번호를 입력 중", e);
-    } catch (DatabaseException e) {
-      log.warn("잘못된 유저 정보로인한 데이터베이스 접속 실패", e);
-    } catch (Exception e) {
-      log.error("데이터베이스 내 알 수 없는 오류 발생 - 원인 분석 및 대응 필요", e);
+    } catch (CustomException e) {
+      log.error("[{}] (Priority: {}) {}", e.getType(), e.getPriority(), e.getMessage(), e);
     }
   }
 }
