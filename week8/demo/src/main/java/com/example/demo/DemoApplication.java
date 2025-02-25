fdf58ecb64d7e143d6256fe7c3517e105623284e
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-25 22:11:05 +0900

    실습 3-26: SOLID 원칙 중 1) Single Responsibility 단일 책임 원칙 적용 전

--- week8/demo/src/main/java/com/example/demo/DemoApplication.java
+++ week8/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,23 +1,15 @@
 package com.example.demo;
 
+import com.example.demo.principles.Ramen;
 import com.example.demo.type.MessageType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
-  public static void createMessage(String message) {
-    MessageType messageType = MessageType.findByName(message);
-    System.out.println("당신에게 메세지를 보냅니다. : " + messageType.getMessage());
-    System.out.println("다음으로부터 전송되었습니다. : " + messageType.getMedia().getSender());
-  }
-
   public static void main(String[] args) {
-//    createMessage("안녕하세요.");
-    createMessage("LOVE");
-    createMessage("THANKS");
-//    createMessage("안녕히가세요.");
-    createMessage("HATES");
+    Ramen ramen = new Ramen();
+    ramen.make();
   }
 
 }
