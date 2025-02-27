a1e0c5b9fff4eb656cf275c0d926cf29962fbf16
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-27 19:11:35 +0900

    실습 5-7: ErrorController 인터페이스 커스텀 구현체 2) ModelAndView 반환

--- week10/demo/src/main/java/com/example/demo/controller/CustomErrorController.java
+++ week10/demo/src/main/java/com/example/demo/controller/CustomErrorController.java
@@ -7,21 +7,32 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CustomErrorController implements ErrorController {
 
   @RequestMapping(value = "/error", method = RequestMethod.GET)
-  public String handleError(HttpServletRequest request) {
+  public ModelAndView handleError(HttpServletRequest request) {
+    ModelAndView modelAndView = new ModelAndView();
+
     Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
     if (status != null) {
       Integer statusCode = Integer.valueOf(status.toString());
+
+      // HttpStatus.NOT_FOUND.value()는 NOT_FOUND의 값(404)를 반환
       if (statusCode == HttpStatus.NOT_FOUND.value()) {
-        return "error/404";
+        modelAndView.addObject("status", statusCode);
+        // HttpStatus.NOT_FOUND.getReasonPhrase())는 Not Found 라는 문자열 반환
+        modelAndView.addObject("reason", HttpStatus.NOT_FOUND.getReasonPhrase());
       } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-        return "error/500";
+        modelAndView.addObject("status", statusCode);
+        modelAndView.addObject("reason", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
       }
     }
-    return "error";
+    // 오류 페이지 설정 (뷰 이름을 "error"로 설정하여 src/main/resources/templates/error.html을 찾아 렌더링)
+    modelAndView.setViewName("error");
+    return modelAndView;
   }
 }
