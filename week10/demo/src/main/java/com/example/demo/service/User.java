e996d99ca49c3e85afeedd7125586bdf2b523916
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-27 23:54:19 +0900

    실습 5-13: @Service Bean 객체 생성 후 User 실시간 조회 및 Model 로의 활용

---
+++ week10/demo/src/main/java/com/example/demo/service/User.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+public class User {
+
+  Integer id;
+  String name;
+  Integer age;
+  String job;
+  String specialty;
+
+}
