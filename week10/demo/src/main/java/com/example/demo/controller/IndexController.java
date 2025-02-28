74f7e83e20c1bb52962654b76722775bcc3f781c
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-28 16:37:12 +0900

    실습 5-24: @RestController = 메서드 단위의 @ResponseBody + 클래스 단위의 @Controller

--- week10/demo/src/main/java/com/example/demo/controller/IndexController.java
+++ week10/demo/src/main/java/com/example/demo/controller/IndexController.java
@@ -1,17 +1,16 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+// @Controller + @ResponseBody = @RestController
+@RestController
 public class IndexController {
 
   // @ResponseBody는 컨트롤러의 반환 값을 HTTP 응답 본문(body)에 직접 넣어서 반환하는 역할을 함. 즉, 뷰를 찾지 않고 데이터 자체를 반환
   // "index" 라는 문자열 자체가 클라이언트(브라우저)로 전송
   @RequestMapping(value = "/", method = RequestMethod.GET)
-  @ResponseBody
   public String index() {
     return "index";
 //      return "index.html";
