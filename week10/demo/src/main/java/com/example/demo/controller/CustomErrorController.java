8d6334587d0d538d0fafe842a96f5583ca019c18
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-02-28 16:33:23 +0900

    실습 5-23: CustomErrorController 수정 : 406 등 모든 상태코드 처리가능하도록 확장 구현 및 @Getter 원상 복구

--- week10/demo/src/main/java/com/example/demo/controller/CustomErrorController.java
+++ week10/demo/src/main/java/com/example/demo/controller/CustomErrorController.java
@@ -5,34 +5,21 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CustomErrorController implements ErrorController {
 
   @RequestMapping(value = "/error", method = RequestMethod.GET)
-  public ModelAndView handleError(HttpServletRequest request) {
-    ModelAndView modelAndView = new ModelAndView();
-
+  public String handleError(HttpServletRequest request, Model model) {
     Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
-    if (status != null) {
-      Integer statusCode = Integer.valueOf(status.toString());
+    Integer statusCode = Integer.valueOf(status.toString());
 
-      // HttpStatus.NOT_FOUND.value()는 NOT_FOUND의 값(404)를 반환
-      if (statusCode == HttpStatus.NOT_FOUND.value()) {
-        modelAndView.addObject("status", statusCode);
-        // HttpStatus.NOT_FOUND.getReasonPhrase())는 Not Found 라는 문자열 반환
-        modelAndView.addObject("reason", HttpStatus.NOT_FOUND.getReasonPhrase());
-      } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-        modelAndView.addObject("status", statusCode);
-        modelAndView.addObject("reason", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
-      }
-    }
-    // 오류 페이지 설정 (뷰 이름을 "error"로 설정하여 src/main/resources/templates/error.html을 찾아 렌더링)
-    modelAndView.setViewName("error");
-    return modelAndView;
+    model.addAttribute("status", statusCode);
+    model.addAttribute("reason", HttpStatus.valueOf(statusCode).getReasonPhrase());
+    return "error";
   }
 }
