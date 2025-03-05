539bb45fbf8e67539943f80491363f01ecff27cc
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-03-05 22:22:02 +0900

    실습 5-44: @JsonProperty : 결과 객체 JSON 반환 시 특정 프로퍼티명 변경 반환

--- week10/demo/src/main/java/com/example/demo/controller/dto/UserResponseDto.java
+++ week10/demo/src/main/java/com/example/demo/controller/dto/UserResponseDto.java
@@ -3,6 +3,7 @@ package com.example.demo.controller.dto;
 import com.example.demo.service.User;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,7 +15,10 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
 public class UserResponseDto { // UserResponseDto 사용하는 이유 : 데이터 유출 방지, 필요한 데이터 전송, API 응답 형식을 명확하게 정의가능
 
+  // 응답 받은 객체의 필드명이 변환된다.
+  @JsonProperty("UserId")
   Integer id;
+  @JsonProperty("username")
   String name;
   Integer age;
   String job;
