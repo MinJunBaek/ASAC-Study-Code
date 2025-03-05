78a88153ef01ac41856c6eeaab7210fc20daf2e9
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-03-05 22:26:42 +0900

    실습 5-45: @JsonPropertyOrder : 결과 객체 JSON 반환 시 프로퍼티 노출 순서 결정

--- week10/demo/src/main/java/com/example/demo/controller/dto/UserResponseDto.java
+++ week10/demo/src/main/java/com/example/demo/controller/dto/UserResponseDto.java
@@ -4,21 +4,22 @@ import com.example.demo.service.User;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
+@JsonPropertyOrder({"userName", "userId"}) // 프로퍼티(필드) 노출 순서 결정(대소문자 구별을 확실히 해야함)
 @Getter
 @JsonInclude(Include.NON_NULL)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
 public class UserResponseDto { // UserResponseDto 사용하는 이유 : 데이터 유출 방지, 필요한 데이터 전송, API 응답 형식을 명확하게 정의가능
 
-  // 응답 받은 객체의 필드명이 변환된다.
-  @JsonProperty("UserId")
+  @JsonProperty("userId")
   Integer id;
-  @JsonProperty("username")
+  @JsonProperty("userName")
   String name;
   Integer age;
   String job;
