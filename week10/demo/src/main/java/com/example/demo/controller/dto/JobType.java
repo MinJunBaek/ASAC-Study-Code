6c2fe0c09bde58d4dce95c60fb283e67a85a414a
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-03-06 14:43:14 +0900

    실습 5-56: Enum 요청(역직렬화)시: Enum 의 특정 필드로 요청에서 Enum 지정 시 @JsonCreator(DELEGATING) - job 이름 안맞춰도 됨

--- week10/demo/src/main/java/com/example/demo/controller/dto/JobType.java
+++ week10/demo/src/main/java/com/example/demo/controller/dto/JobType.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 import java.util.Arrays;
 import java.util.List;
 import lombok.AccessLevel;
@@ -18,13 +19,18 @@ public enum JobType {
   String name;
   List<String> titles;
 
-  @JsonCreator
-  public static JobType deserialize(String job) {
+  // Json데이터를 자바 객체로 변환할때 사용
+  // DELEGATING 모드를 사용하면 JSON의 단일 값을 특정 필드에 매핑하도록 설정할수 있음.
+  // 즉, JSON에서 하나의 값을 전체 객체로 변환할 때 특정 필드에 전달하여 처리하도록 위임 하는 방식
+  // DELEGATING는 특정 필드를 deserialize(String name)를 통해 자동으로 매핑해줌
+  // 자동 매핑이 싫다면 PROPERTIES를 사용하고 deserialize(@JsonProperty("name") String name)이라고 작성하면 됨.
+  @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+  public static JobType deserialize(String name) {
     for (JobType each : JobType.values()) {
-      if (each.getName().equals(job)) {
+      if (each.getName().equals(name)) {
         return each;
       }
     }
-    throw new RuntimeException("JobType 내 해당하는 Enum 이 존재하지 않습니다. name : " + job);
+    throw new RuntimeException("JobType 내 해당하는 Enum 이 존재하지 않습니다. name : " + name);
   }
 }
