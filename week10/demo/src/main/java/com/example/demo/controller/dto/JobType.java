53fce105a63e3b83834c139875e1dcf3b3e5ab07
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-03-06 14:15:27 +0900

    실습 5-55: Enum 요청(역직렬화)시: Enum 의 특정 필드로 요청에서 Enum 지정 시 @JsonCreator - job 이름을 맞춰야함

--- week10/demo/src/main/java/com/example/demo/controller/dto/JobType.java
+++ week10/demo/src/main/java/com/example/demo/controller/dto/JobType.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import java.util.Arrays;
 import java.util.List;
 import lombok.AccessLevel;
@@ -12,11 +12,19 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public enum JobType {
-  // @JsonProperty("Developer 개발자") 아무것도 설정하지 않으면 기본값(Enum의 이름)이 출력된다.
   DEVELOPER("Developer", Arrays.asList("Frontend", "Backend")),
   ENGINEER("Engineer", Arrays.asList("DevOps", "SRE"));
 
   String name;
   List<String> titles;
 
+  @JsonCreator
+  public static JobType deserialize(String job) {
+    for (JobType each : JobType.values()) {
+      if (each.getName().equals(job)) {
+        return each;
+      }
+    }
+    throw new RuntimeException("JobType 내 해당하는 Enum 이 존재하지 않습니다. name : " + job);
+  }
 }
