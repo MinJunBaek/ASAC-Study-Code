a26f80932dca64c4c9fd203ab8055fd90320dc8d
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-03-06 11:47:10 +0900

    실습 5-51: Enum 반환(직렬화) 시 : Enum 내 특정 필드만 출력 - @JsonValue 필드

--- week10/demo/src/main/java/com/example/demo/controller/dto/JobType.java
+++ week10/demo/src/main/java/com/example/demo/controller/dto/JobType.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import com.fasterxml.jackson.annotation.JsonValue;
 import java.util.Arrays;
 import java.util.List;
 import lombok.AccessLevel;
@@ -9,19 +8,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
-// java의 enum은 JSON 반환 시 이름 문자열만 반환됨.
-// Shape.OBJECT를 하면 객체로 변환되어 반환됨.
-@JsonFormat(shape = Shape.OBJECT)
-// Shape.OBJECT를 사용하여 객체로 변환할때 해당 객체의 값을 가져오기 위해 사용
 @Getter
 @AllArgsConstructor
-// final로 선언한 이유는 불변이어야 하기 때문에
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public enum JobType {
-  // 미리 생성해 놓은 인스턴스들의 집합(ENUM)
   DEVELOPER("Developer", Arrays.asList("Frontend", "Backend")),
   ENGINEER("Engineer", Arrays.asList("DevOps", "SRE"));
 
+  // 출력시 name필드만 출력된다.
+  @JsonValue
   String name;
   List<String> titles;
 }
