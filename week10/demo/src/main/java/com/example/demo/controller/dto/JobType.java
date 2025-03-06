cebd3f02f85c56632edfaf059cc8bb896aca07d0
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-03-06 12:00:51 +0900

    실습 5-53: Enum 반환(직렬화) 시 : 원하는 형태로 출력 시 - @JsonProperty

--- week10/demo/src/main/java/com/example/demo/controller/dto/JobType.java
+++ week10/demo/src/main/java/com/example/demo/controller/dto/JobType.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller.dto;
 
-import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Arrays;
 import java.util.List;
 import lombok.AccessLevel;
@@ -12,16 +12,12 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public enum JobType {
+  @JsonProperty("Developer 개발자") // 등록하려면 job 부분에 Developer 개발자 라고 입력해야함
   DEVELOPER("Developer", Arrays.asList("Frontend", "Backend")),
+  @JsonProperty("Engineer 엔지니어")
   ENGINEER("Engineer", Arrays.asList("DevOps", "SRE"));
 
   String name;
   List<String> titles;
 
-  // 객체가 JSON으로 변환될 때 사용될 값을 지정하는 Jackson 어노테이션
-  // 보통 필드나 메서드에 적용가능한데 메서드에 적용하면 값을 원하는 형식으로 포멧팅 하여 출력 할수 있다.
-  @JsonValue
-  public String serialize() {
-    return String.format("명칭 : %s | 종류 : %s", this.name, titles.toString());
-  }
 }
